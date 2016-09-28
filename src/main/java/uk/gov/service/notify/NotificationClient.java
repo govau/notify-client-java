142e3b141b8eff362b32a84d930ea21364602535
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2016-09-28 14:47:09 +0100

    Updated as per review comments.

--- src/main/java/uk/gov/service/notify/NotificationClient.java
+++ src/main/java/uk/gov/service/notify/NotificationClient.java
@@ -43,8 +43,8 @@ public class NotificationClient implements NotificationClientApi {
      * The sendEmail method will create an HTTPS POST request. A JWT token will be created and added as an Authorization header to the request.
      *
      * @param templateId Find templateId by clicking API info for the template you want to send
-     * @param to the email address
-     * @param personalisation optional HashMap representing the placeholders for the template if any. For example, key=name value=Bob
+     * @param to The email address
+     * @param personalisation Optional HashMap representing the placeholders for the template if any. For example, key=name value=Bob
      * @return <code>NotificationResponse</code>
      * @throws NotificationClientException
      */
@@ -56,8 +56,8 @@ public class NotificationClient implements NotificationClientApi {
      * The sendSms method will create an HTTPS POST request. A JWT token will be created and added as an Authorization header to the request.
      *
      * @param templateId Find templateId by clicking API info for the template you want to send
-     * @param to the mobile phone number
-     * @param personalisation optional HashMap representing the placeholders for the template if any. For example, key=name value=Bob
+     * @param to The mobile phone number
+     * @param personalisation Optional HashMap representing the placeholders for the template if any. For example, key=name value=Bob
      * @return <code>NotificationResponse</code>
      * @throws NotificationClientException
      */
@@ -99,10 +99,10 @@ public class NotificationClient implements NotificationClientApi {
     }
 
     /**
-     * The getNotificationById will return a <code>Notification</code> for a given notification id.
+     * The getNotificationById method will return a <code>Notification</code> for a given notification id.
      * The id is can be retrieved from the <code>NotificationResponse</code> of a <code>sendEmail</code> or <code>sendSms</code> request.
      *
-     * @param notificationId the id of the notification.
+     * @param notificationId The id of the notification.
      * @return <code>Notification</code>
      * @throws NotificationClientException
      */
@@ -138,11 +138,11 @@ public class NotificationClient implements NotificationClientApi {
     }
 
     /**
-     * The getNotifications will create a GET HTTPS request to retrieve all the notifications.
+     * The getNotifications method will create a GET HTTPS request to retrieve all the notifications.
      *
-     * @param status if status is not null notifications will only return notifications for the given status.
+     * @param status If status is not null notifications will only return notifications for the given status.
      *               Possible statuses are created|sending|delivered|permanent-failure|temporary-failure|technical-failure
-     * @param notification_type if notification_type is not null only notification of the given status will be returned.
+     * @param notification_type If notification_type is not null only notification of the given status will be returned.
      *                          Possible notificationTypes are sms|email
      * @return <code>NotificationList</code>
      * @throws NotificationClientException
